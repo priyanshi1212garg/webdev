88265b53919bd01020e4bff44e2a2e23d5c7d752
priyanshi1212garg <106342929+priyanshi1212garg@users.noreply.github.com>
2022-07-24 01:33:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
 
-ReactDOM.render(
-<React.StrictMode>
-	<App />
-</React.StrictMode>,
-document.getElementById('root')
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/music' activeStyle>
+			Music 
+		</NavLink>
+		<NavLink to='/tech' activeStyle>
+			Tech 
+		</NavLink>
+		<NavLink to='/sports' activeStyle>
+			Sports 
+		</NavLink>
+		<NavLink to='/contact' activeStyle>
+			Contact us
+		</NavLink>
+		<NavLink to='/testimonials' activeStyle>
+			Testimonials
+		</NavLink>
+		
+		{/* Second Nav */}
+		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+		<NavBtn>
+		<NavBtnLink to='/Form'>Register</NavBtnLink>
+		</NavBtn>
+	</Nav>
+	</>
 );
+};
+
+export default Navbar;
